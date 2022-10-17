77ad6bee1a75b43bcf67a6f265198780f2a0b024
opa334 <opa334@users.noreply.github.com>
2022-10-17 19:35:06 +0200

    Fix apps crashing after launching the u0Launcher app in jailbroken mode

--- U0LRootViewController.m
+++ U0LRootViewController.m
@@ -1,9 +1,26 @@
 #import "U0LRootViewController.h"
+#import <mach-o/dyld.h>
 
 @interface LSApplicationWorkspace : NSObject
 + (instancetype)defaultWorkspace;
 - (BOOL)openApplicationWithBundleID:(NSString*)appId;
 @end
+
+BOOL isAlreadyJailbroken(void)
+{
+	for (uint32_t i = 0; i < _dyld_image_count(); i++)
+	{
+		const char *pathC = _dyld_get_image_name(i);
+		NSString* path = [NSString stringWithUTF8String:pathC];
+
+		if([path isEqualToString:@"/usr/lib/substitute-inserter.dylib"])
+		{
+			return YES;
+		}
+	}
+
+	return NO;
+}
 
 @implementation U0LRootViewController
 
@@ -23,6 +40,12 @@
 - (void)viewDidLoad
 {
 	[super viewDidLoad];
+
+	if(isAlreadyJailbroken())
+	{
+		exit(0);
+		return;
+	}
 
 	// Ping amfid, which makes it launch (which unc0ver needs)
 	xpc_connection_t xpcConnection = xpc_connection_create_mach_service("com.apple.MobileFileIntegrity", 0, XPC_CONNECTION_MACH_SERVICE_PRIVILEGED);
